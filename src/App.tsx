691908b5c18468bafe733a7c18ed59d98b8037e3
Walker <walkerrm@gmail.com>
2023-08-14 20:32:35 -0400

    add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 // import { useState } from 'react'
 // import "./App.css";
-import { Step1 } from "./pages/step1/step1";
+// import { Step1 } from "./pages/step1/step1";
+import { CreateNew } from "./pages/createNew/createNew";
+import { ModeToggle } from "./components/mode-toggle";
+import { ThemeProvider } from "@/components/theme-provider";
 
 function App() {
   return (
-    <>
-      {/* <MainLayout> */}
-      {/* <Navigation /> */}
-      <Step1 />
-      {/* </MainLayout> */}
-    </>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="flex justify-center py-6">
+        <div>
+          <div className="flex justify-end">
+            <ModeToggle />
+          </div>
+          <div className="flex justify-center max-w-lg">
+            <div className="flex flex-col">
+              <CreateNew />
+            </div>
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
 
